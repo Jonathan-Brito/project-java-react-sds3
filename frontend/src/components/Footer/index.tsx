058f8996a36665067577bae8ff9557ba04e850e3
Jonathan-Brito <devjonathanbrito@gmail.com>
2021-05-05 21:25:02 -0300

    Added

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,10 +1,8 @@
-
-
-function Footer() {
+const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/devjonathanbrito" target="_blank" rel="noreferrer">Nelio Alves</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/devjonathanbrito" target="_blank" rel="noreferrer">Jonathan Brito</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br/>
                 Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
