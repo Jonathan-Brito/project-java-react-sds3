561216588f3e5b71d9dca05551e046c9469e449d
Jonathan-Brito <devjonathanbrito@gmail.com>
2021-05-07 00:07:26 -0300

    Pagination

--- backend/src/main/java/com/brito/vendas/service/SaleService.java
+++ backend/src/main/java/com/brito/vendas/service/SaleService.java
@@ -1,8 +1,30 @@
 package com.brito.vendas.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.brito.vendas.dto.SaleDTO;
+import com.brito.vendas.entities.Sale;
+import com.brito.vendas.repositories.SaleRepository;
+import com.brito.vendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
 
 }
