51e4c2f4c805c02a242f9f7fcbf841fb1783dac9
Jonathan-Brito <devjonathanbrito@gmail.com>
2021-05-08 17:35:31 -0300

    First homolg

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/devjonathanbrito" target="_blank" rel="noreferrer">Jonathan Brito</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br/>
-                Aplicação desenvolvida durabte a semana DevSuperior: <a href="https://www.linkedin.com/in/jonathan-brito-803960193/" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                Aplicação desenvolvida durante a semana DevSuperior: <a href="https://www.linkedin.com/in/jonathan-brito-803960193/" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
     );
